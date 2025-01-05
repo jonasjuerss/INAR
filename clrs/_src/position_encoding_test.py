e020d816eeb89fd6ffc6b02ed58dc4917f8f4e9e
jonasjuerss <jonas.juerss@tum.de>
2025-01-05 12:45:35 +0100

    test: fix tests for positional encoding

--- clrs/_src/position_encoding_test.py
+++ clrs/_src/position_encoding_test.py
@@ -6,7 +6,7 @@ import jax
 import jax.numpy as jnp
 
 
-def positional_encoding(seq_len, d_model = 512):            
+def positional_encoding(seq_len, d_model = 512):
     position = jnp.arange(0, seq_len)[:, None]  # Shape (seq_len, 1)
     div_term = jnp.exp(jnp.arange(0, d_model, 2) * -(jnp.log(10000.0) / d_model))  # Shape (d_model / 2,)
     positional_encoding = jnp.zeros((seq_len, d_model))
@@ -25,12 +25,16 @@ class TestPositionalEncoding(unittest.TestCase):
 
     def test_known_values(self):
         # Test if the positional encoding gives known values for simple inputs
-        seq_length = 1
+        seq_length = 3
         embed_dim = 4
-        pe = positional_encoding(seq_length, embed_dim)   
-        pe = [pe.reshape((pe.shape[-1]))]
-        expected = jnp.array([[0.0, 1.0, jnp.sin(1 / 10000**(2 / embed_dim)), jnp.cos(1 / 10000**(2 / embed_dim))]])
-        assert_almost_equal(pe, expected, decimal=2) #### if we set decimal > 2, this test fails
+        pe = positional_encoding(seq_length, embed_dim)
+
+        div_1 = 10000 ** (2 / embed_dim)
+
+        expected = jnp.array([[0.0, 1.0, 0.0, 1.0],
+                              [jnp.sin(1 / 1), jnp.cos(1 / 1), jnp.sin(1 / div_1), jnp.cos(1 / div_1)],
+                              [jnp.sin(2 / 1), jnp.cos(2 / 1), jnp.sin(2 / div_1), jnp.cos(2 / div_1)]])
+        assert jnp.allclose(pe, expected)
 
     def test_symmetry(self):
         # Check if even indices use sin and odd indices use cos
