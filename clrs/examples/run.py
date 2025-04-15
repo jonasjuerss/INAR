1a46dbee37cc3f4b251cc7d9a21d52f50a8be7ad
Manasvi Aggarwal <manasvi.ducompsci@gmail.com>
2025-04-15 17:37:28 +0530

    Fix bugs in run.py and nets.py

--- clrs/examples/run.py
+++ clrs/examples/run.py
@@ -30,7 +30,7 @@ import requests
 import tensorflow as tf
 
 
-flags.DEFINE_list('algorithms', ['dfs'], 'Which algorithms to run.')
+flags.DEFINE_list('algorithms', ['bfs'], 'Which algorithms to run.')
 flags.DEFINE_list('train_lengths', ['4', '7', '11', '13', '16'],
                   'Which training sizes to use. A size of -1 means '
                   'use the benchmark dataset.')
@@ -42,6 +42,7 @@ flags.DEFINE_integer('length_needle', -8,
                      'A value of 0 means use always 1/4 of the length of '
                      'the haystack (the default sampler behavior).')
 flags.DEFINE_integer('seed', 42, 'Random seed to set')
+
 flags.DEFINE_boolean('random_pos', True,
                      'Randomize the pos input common to all algos.')
 flags.DEFINE_boolean('enforce_permutations', True,
@@ -111,15 +112,13 @@ flags.DEFINE_enum('processor_type', 'triplet_gmpnn',
                    'triplet_gpgn', 'triplet_gpgn_mask', 'triplet_gmpnn'],
                   'Processor type to use as the network P.')
 
-
 flags.DEFINE_string('checkpoint_path', '/tmp/CLRS30',
                     'Path in which checkpoints are saved.')
 flags.DEFINE_string('dataset_path', '/tmp/CLRS30',
                     'Path in which dataset is stored.')
+
 flags.DEFINE_boolean('freeze_processor', False,
                      'Whether to freeze the processor of the model.')
-
-
 
 FLAGS = flags.FLAGS
 
@@ -439,8 +438,6 @@ def main(unused_argv):
       use_ln=FLAGS.use_ln,
       nb_triplet_fts=FLAGS.nb_triplet_fts,
       nb_heads=FLAGS.nb_heads,
-      gated = FLAGS.gated,
-      
   )
   model_params = dict(
       processor_factory=processor_factory,
@@ -457,9 +454,6 @@ def main(unused_argv):
       hint_teacher_forcing=FLAGS.hint_teacher_forcing,
       hint_repred_mode=FLAGS.hint_repred_mode,
       nb_msg_passing_steps=FLAGS.nb_msg_passing_steps,
-      time_encoding = FLAGS.time_encoding,
-      positional_encoding = FLAGS.positional_encoding,
-      baseline = FLAGS.baseline, 
       )
 
   eval_model = clrs.models.BaselineModel(
